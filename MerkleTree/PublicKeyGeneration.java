8277a68e2c889a0054e74bec856f26d8d12fdf49
Frolov-Viacheslav <frolov_vyacheslav21@mail.ru>
2018-02-27 23:30:14 +0200

    Update PublicKeyGeneration.java

--- MerkleTree/PublicKeyGeneration.java
+++ MerkleTree/PublicKeyGeneration.java
@@ -2,7 +2,7 @@ package MerkleTree;
 import Config.*;
 
 public class PublicKeyGeneration {
-   // PRG prg = new PRG();
+   PRG prg = new PRG();
    WOTS_PLUS.P_KeyPairGeneration kpg = new WOTS_PLUS.P_KeyPairGeneration();
 
     public static String [][] keysArray;
@@ -22,8 +22,9 @@ public class PublicKeyGeneration {
     public static String root;
     public static String[][] tree;
     public static int countLayer;
+    public static String[] bitMask;
 
-    public String treeBilding(String [][] keysArray, Integer N){ // Tree building with L-Tree
+    public void treeBuilding(String [][] keysArray, Integer N, Integer s){ // Tree building with L-Tree
         countLayer = (int)Math.ceil(Binarylog.binlog((double) N)) + 1;
         if(keysArray[1].length % 2 == 0)
             tree = new String[countLayer][N];
@@ -32,26 +33,40 @@ public class PublicKeyGeneration {
         for(int i = 0; i < N; i++) {
             tree[0][i] = keysArray[1][i];
         }
+        creationBitMask(s);
+        calculateRoot(N, s);
+        printTree(N);
+    }
+
+    public String calculateRoot(Integer N, Integer s){
         int k = 0;
         for (int i = 1; i < countLayer; i++) {
             for (int j = 0; j < N / (Math.pow(2, i - 1)); j += 2) {
                 if(tree[i - 1][j + 1] != null)
-                    tree[i][k] = MD5HEX.md5Custom(tree[i - 1][j] + tree[i - 1][j + 1]);
+                    tree[i][k] = MD5HEX.md5Custom(kpg.xor(tree[i - 1][j], bitMask[i * 2], s/4)  + kpg.xor(tree[i - 1][j + 1], bitMask[i * 2 + 1], s/4));
                 else
                     tree[i][k] = tree[i - 1][j];
                 k++;
             }
             k = 0;
         }
+        root = tree[countLayer-1][0];
+        return root;
+    }
 
+    public  void printTree(Integer N){
         for (int i = 0; i < countLayer; i++) {
             for (int j = 0; j < N; j++) {
                 System.out.print(tree[i][j] + "\t");
             }
             System.out.println();
         }
+    }
 
-        root = tree[countLayer-1][0];
-        return root;
+    public void creationBitMask(Integer s){
+        bitMask = new String [countLayer * 2];
+        for (int i = 0; i < bitMask.length; i++) {
+            bitMask[i] = prg.RandomN(s/4);
+        }
     }
 }
